8fcf786bf80d9009d39d15d5bd6c4f5aef908a57
Студенческая <@rbiu.local>
2025-10-20 13:03:39 +0500

    Проверка API, разобраться почему не загружаются картинки собак.

--- shared/card.js
+++ shared/card.js
@@ -1,10 +1,31 @@
+'use client'
 import Image from "next/image";
-import Pict from "../public/img/dog-1.jpg"
+import placeholder from "../public/loading.jpg";
+import { useState, useEffect } from "react";
 
 export const Card=()=>{
+    const [imageSrc, setImageSrc] = useState(placeholder);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            try{
+                const responce = await fetch
+                ('https://dog.ceo/api/breeds/image/random');
+                const data = await responce.json();
+                setImageSrc(data.message);
+            } catch (error) {
+                console.error('Error', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchImage();
+        },
+    []);
     return(
-        <div>
-            <Image className="rounded-4xl" src={Pict} alt="ADog"/>
+        <div className="w-[263px] h-[189px]">
+            <Image className="rounded-[50px]" src={imageSrc} height={189} width={263} alt="ADog"/>
         </div>
     )
 }
--- widgets/breeds.js
+++ widgets/breeds.js
@@ -2,6 +2,7 @@ import { Card } from "../shared/card"
 export const Breeds=()=>{
     return(
         <div> 
+            <span className="font-[Inter] text-[40px] text-[#4d86ff] font-bold mb-2">Породы</span>
             <Card/>
         </div>
     )
